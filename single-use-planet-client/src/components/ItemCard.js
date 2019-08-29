1fb6fffdc2f89f3f449d0c5807a9d885190e5838
Aellon Krider <aellonkrider@gmail.com>
2019-08-29 15:52:16 -0400

    Update ItemCard.js with correct ClassName syntax

--- single-use-planet-client/src/components/ItemCard.js
+++ single-use-planet-client/src/components/ItemCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ItemCard = ({ item }) => (
 			<div key={item.id}> 
-				<h1 class="ItemCardTitle">{item.name}</h1>
-				<img class="ItemCardImg"src={item.img_url} alt={item.name}/>
+				<h1 className="ItemCardTitle">{item.name}</h1>
+				<img className="ItemCardImg" src={item.img_url} alt={item.name}/>
 				<p>Material: {item.material}</p>
 				<p>Alternative Options: {item.alternative}</p>
 				<p>How To Recycle: {item.instructions}</p>
