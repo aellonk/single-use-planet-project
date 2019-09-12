f8fbe34f69f30ae8b5492b19b8212b73e8cf32b2
Aellon Krider <aellonkrider@gmail.com>
2019-09-12 15:18:36 -0400

    Update Items.js and App.css with new const and class name

--- single-use-planet-client/src/containers/Items.js
+++ single-use-planet-client/src/containers/Items.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+import ItemName from '../components/ItemName';
 import ItemCard from '../components/ItemCard';
 import { getItems } from '../actions/items';
+import SearchInput from '../components/SearchInput';
 
 
 class Items extends Component {
@@ -11,12 +14,14 @@ class Items extends Component {
 	}
 	
 	render() {
+		
 		return (
-
 		<div>
+			<SearchInput/>
+			<h1>{this.props.items.filter(item => item.name.includes(SearchInput.input))}</h1>
 			<h3>What To Do With:</h3>
 
-			{this.props.items.map(item => <ItemCard key={item.id} item={item} />)}
+			{this.props.items.map(item =><ItemName key={item.id} item={item} />)}
 		</div>
 		);
 	}
