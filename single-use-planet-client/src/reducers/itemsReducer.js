afab2c9a49f72a603d6cdf0a07f5ff28a14951da
Aellon Krider <aellonkrider@gmail.com>
2019-09-12 16:58:11 -0400

    Update itemsReducer with correct syntax

--- single-use-planet-client/src/reducers/itemsReducer.js
+++ single-use-planet-client/src/reducers/itemsReducer.js
@@ -2,7 +2,8 @@ const itemsReducer = (state = { items: []}, action) => {
 	switch(action.type) {
 		case 'GET_ITEMS_SUCCESS':
 			return {items: action.items};
-
+		case 'GET_SEARCHED_ITEMS':
+			return {items: action.items}
 		default:
 			return state;
 	}
