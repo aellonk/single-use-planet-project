d4d03a67020b7fd34ecd5b19e076b153cd541e08
Aellon Krider <aellonkrider@gmail.com>
2019-10-08 21:51:11 -0400

    Add About component

---
+++ single-use-planet-client/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = ()=> (
+			<div> 
+				<h1>Single Use Planet is a project to help people figure out what to do with the things that no longer serve them and to aid in waste diversion for a more sustainable lifestyle.</h1>
+			</div>
+)
+
+export default About;
