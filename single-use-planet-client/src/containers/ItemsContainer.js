2095db3c876827b4ad61b3d9d7d7069301d3ada4
Aellon Krider <aellonkrider@gmail.com>
2019-11-29 19:14:04 -0500

    Add ItemsContainer container to render search input and Items components

---
+++ single-use-planet-client/src/containers/ItemsContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import SearchInput from '../components/SearchInput';
+import Items from '../containers/Items';
+
+class ItemsContainer extends React.Component {
+	
+	render() {
+		return (
+		<div>
+		 	<h1>Items Container</h1>
+		 	<SearchInput />
+			<Items />
+		</div>
+		);
+	}
+	
+}
+
+export default ItemsContainer;
