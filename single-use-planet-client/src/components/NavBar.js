af872482d77eb7198376831d44ef5337f82a3c66
Aellon Krider <aellonkrider@gmail.com>
2019-12-24 18:41:27 -0500

    Add react-bootstrap and update navbar with it

--- single-use-planet-client/src/components/NavBar.js
+++ single-use-planet-client/src/components/NavBar.js
@@ -1,24 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const NavBar = () => {
   return (
-    <nav>
-        <ul>
-          <li>
-            <NavLink exact to="/items">Browse Items</NavLink>
-          </li>
-          <li>
-             <NavLink exact to="/helpers">Zero Waste Helpers</NavLink>
-          </li>
-          <li>
-            <NavLink exact to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink exact to="/feedback">Feedback</NavLink>
-          </li>
-        </ul>
-    </nav>
+    <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="/">Single Use Planet</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/items">Browse Items</Nav.Link>
+        <Nav.Link href="/resources">Zero Waste Resources</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/feedback">Feedback</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+</Navbar>
   )
 }
 
--- single-use-planet-client/src/containers/App.js
+++ single-use-planet-client/src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ItemsContainer from './ItemsContainer';
 import './App.css';
 import About from '../components/About';
-import Helpers from '../components/Helpers';
+import Resources from '../components/Resources';
 import ItemCard from '../components/ItemCard';
 import NavBar from '../components/NavBar';
 import	FeedbackForm from '../components/FeedbackForm';
@@ -14,11 +14,10 @@ class App extends Component {
 	render() {
 		return (
 			<div> 
-				App Container
 				<NavBar/>
 				<Switch>
 	              <Route exact path= '/items' component={ ItemsContainer }/>
-	              <Route exact path= '/helpers' component={ Helpers }/>
+	              <Route exact path= '/resources' component={ Resources }/>
 	              <Route exact path= '/about' component={ About }/>
 	              <Route exact path= '/feedback' component={ FeedbackForm }/>
 	            </Switch>
