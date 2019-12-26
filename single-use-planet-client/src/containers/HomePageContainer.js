8973c680e3aa422c3ca534a046b9184df2588ad6
Aellon Krider <aellonkrider@gmail.com>
2019-12-26 13:40:12 -0500

    Update style for homepage container

--- single-use-planet-client/src/containers/HomePageContainer.js
+++ single-use-planet-client/src/containers/HomePageContainer.js
@@ -60,20 +60,22 @@ class HomePageContainer extends React.Component {
 
 			<div className="container marketing">
 			    <div className="row">
-			      <div className="col-lg-4 text-center">
+			      <div className="col-lg-4 text-center featurette">
 			        <img src="https://source.unsplash.com/mWTOR3Rx8l8/" className="bd-placeholder-img-lg featurette-image img-fluid mx-auto"/>
 			        <h2>Recycle Electronics</h2>
+			        <p>Lots of items fall into this category including alarm clocks; CD/DVD players and game consoles; small kitchen appliances such as kettles, toasters and blenders; personal grooming products like hairdryers, straighteners, electric toothbrushes and shavers; other items such as lamps, flashlights, vacuum cleaners, phones, radios, TVs, printers, cameras, and smoke alarms.</p>
 			        <p><a className="btn btn-secondary" href="#" role="button">Learn more &raquo;</a></p>
 			      </div>
-			      <div className="col-lg-4 text-center">
+			      <div className="col-lg-4 text-center featurette">
 			        <img src={TerraCycle_Logo_Low_Res} className="bd-placeholder-img-lg featurette-image img-fluid mx-auto"/>
 			        <h2>Check out TerraCycle</h2>
 			        <p>TerraCycle offers a range of national, easy-to-use recycling platforms allowing everyone to #RecycleEverything, as well as Loop, a sustainable shopping experience moving the world away from single-use packaging.</p>
 			        <p><a className="btn btn-secondary" href="https://www.terracycle.com/en-US" target="_blank" rel="noopener noreferrer" role="button">Learn more &raquo;</a></p>
 			      </div>
-			      <div className="col-lg-4 text-center">
-			        <img src="https://source.unsplash.com/mWTOR3Rx8l8/" className="bd-placeholder-img-lg featurette-image img-fluid mx-auto"/>
-			        <h2>Recycle your old gadgets</h2>
+			      <div className="col-lg-4 text-center featurette">
+			        <img src="https://source.unsplash.com/pc00EDmxodI/" className="bd-placeholder-img-lg featurette-image img-fluid mx-auto"/>
+			        <h2>Recycle Christmas Trees</h2>
+			        <p>Wrap up your holiday season and say goodbye to your tree at Mulchfest. NYC Parks and the New York City Department of Sanitation will chop your tree into wood chips that they'll use to nourish trees and make NYC even greener.</p>
 			        <p><a className="btn btn-secondary" href="#" role="button">Learn more &raquo;</a></p>
 			      </div>
 			    </div>
