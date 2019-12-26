6cdf7e271fd5dfcb2f976115213f5c6eefde4a9d
Aellon Krider <aellonkrider@gmail.com>
2019-12-26 14:49:11 -0500

    Update style for ItemCard

--- single-use-planet-client/src/components/ItemCard.js
+++ single-use-planet-client/src/components/ItemCard.js
@@ -5,11 +5,17 @@ const ItemCard = ({item}) => {
 return (
 	<div>
 	<div key={item.id}> 
-		<h1 className="ItemCardTitle">{item.name}</h1>
+		<h1 className="ItemCardTitle text-center">{item.name}</h1>
+		<div className="row m-4">
+		<div className="col-5">
 		<img className="ItemCardImg" src={item.img_url} alt={item.name}/>
-		<p>Material: {item.material}</p>
-		<p>Alternative Options: {item.alternative}</p>
-		<p>How To Recycle: {item.instructions}</p>
+		</div>
+		<div className="col-7">
+		<p><b>Material</b>: {item.material}</p>
+		<p><b>Alternative Options</b>: {item.alternative}</p>
+		<p><b>How To Recycle</b>: {item.instructions}</p>
+		</div>
+		</div>
 	</div>
 	</div>
 	)
