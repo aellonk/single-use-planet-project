aaf5ebffff1b674a0f0bab0ebe1dd54ce55c1cce
Aellon Krider <aellonkrider@gmail.com>
2019-12-31 10:00:43 -0500

    Update About blurb

--- single-use-planet-client/src/reducers/itemsReducer.js
+++ single-use-planet-client/src/reducers/itemsReducer.js
@@ -2,8 +2,7 @@ const itemsReducer = (state = { items: []}, action) => {
 	switch(action.type) {
 		case 'GET_ITEMS_SUCCESS':
 			return {items: action.items};
-		case 'GET_SEARCHED_ITEMS':
-			return {items: action.items}
+	
 		default:
 			return state;
 	}
