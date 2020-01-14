2a3ea9607b2c3b69245f7c2c9047a8eee0aedf5e
Aellon Krider <aellonkrider@gmail.com>
2020-01-13 22:20:40 -0500

    Update Items Container to be a function instead of class

--- single-use-planet-client/src/containers/ItemsContainer.js
+++ single-use-planet-client/src/containers/ItemsContainer.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Items from '../components/Items';
 
 
-class ItemsContainer extends Component {
+const ItemsContainer = () => {
 	
-	render() {
 		return (
 		<div className="container-fluid">
 		 	<h1 className="text-center mt-4">What To Do With...</h1>
 			<Items/>
 		</div>
 		);
-	}
-	
 }
 
 export default ItemsContainer;
