ea0a91459b4fbed2b2d44e13a2c9b907dc928153
Aellon Krider <aellonkrider@gmail.com>
2020-01-13 19:53:59 -0500

    Remove unused imports

--- single-use-planet-client/src/index.js
+++ single-use-planet-client/src/index.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
 import thunk from 'redux-thunk';
 import './index.css';
 import itemsReducer from './reducers/itemsReducer';
