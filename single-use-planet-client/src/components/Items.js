4ab777c21af8755b5b742734f73d145ee62c5848
Aellon Krider <aellonkrider@gmail.com>
2020-04-24 14:54:22 -0400

    Add collapsible buttons for items to display details

--- single-use-planet-client/src/components/Items.js
+++ single-use-planet-client/src/components/Items.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import ItemName from '../components/ItemName';
 import ItemCard from '../components/ItemCard';
 import { getItems } from '../actions/items';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
 
 class Items extends Component {
 
@@ -12,7 +14,8 @@ class Items extends Component {
 	    this.state = {
 	      searched: false,
 	      clicked: false,
-	      item: ''
+		  item: '',
+		  open: false
 	    };
   	}
 
@@ -36,9 +39,27 @@ class Items extends Component {
   		return (
   			<div className="col-12">
 			{this.props.items.map(item => (
-				<Link to={`/items/${item.id}`} key={item.id} onClick={(e) => this.handleClick(e, item) }>
-					<ItemName key={item.id} item={item} />
-				</Link> 
+				<>
+					<Button
+						onClick={() => this.setState({ open: !this.state.open })}
+						aria-controls="example-collapse-text"
+						aria-expanded={this.state.open}
+					>
+						<Link to={`/items/${item.id}`} key={item.id} onClick={(e) => this.handleClick(e, item) }>
+							<ItemName key={item.id} item={item} />
+						</Link> 
+					</Button>
+					<Collapse in={this.state.open}>
+						<div id="example-collapse-text">
+							{this.state.clicked ? <ItemCard item={this.state.item}/> : null}
+						</div>
+					</Collapse>
+				</>
+
+
+
+
+				
 				)
 			)
 			}
@@ -70,8 +91,11 @@ class Items extends Component {
 		<input id="searchfocus" type="text" value={this.state.input} placeholder={"Find an Item"} onChange={this.handleOnChange.bind(this)} />
 		</div>
 		<div className="row mt-4">
+			
 			{this.state.searched ? this.renderSearchedList() : this.renderAll()}
-			{this.state.clicked ? <ItemCard item={this.state.item}/> : null}
+
+					
+					
 		</div>
 		</div>
 		);
