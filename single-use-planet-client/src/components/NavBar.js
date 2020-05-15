196a33afa4a75e560a8acba829f0dfea5934d715
Aellon Krider <aellonkrider@gmail.com>
2020-05-14 22:46:09 -0400

    Add padding to menu items and change name to all uppercase

--- single-use-planet-client/src/components/NavBar.js
+++ single-use-planet-client/src/components/NavBar.js
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 const NavBar = () => {
   return (
     <Navbar expand="lg">
-      <Navbar.Brand href="/">Single-use Planet</Navbar.Brand>
+      <Navbar.Brand href="/">SINGLE-USE PLANET</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
