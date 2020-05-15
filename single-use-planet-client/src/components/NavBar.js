cfdb15310b48a0ad2ca5e34509c07282a4af2557
Aellon Krider <aellonkrider@gmail.com>
2020-05-14 22:31:42 -0400

    Change font to Roboto

--- single-use-planet-client/src/components/NavBar.js
+++ single-use-planet-client/src/components/NavBar.js
@@ -5,17 +5,17 @@ import Nav from 'react-bootstrap/Nav';
 const NavBar = () => {
   return (
     <Navbar expand="lg">
-    <Navbar.Brand href="/">Single-use Planet</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/items">Browse Items</Nav.Link>
-        <Nav.Link href="/resources">Zero Waste Resources</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/items/new">Add Item</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-</Navbar>
+      <Navbar.Brand href="/">Single-use Planet</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/items">Browse Items</Nav.Link>
+          <Nav.Link href="/resources">Zero Waste Resources</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/items/new">Add Item</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
