ab6108fa0f1f4bec0019326566f5da40487f0f7f
Aellon Krider <aellonkrider@gmail.com>
2020-05-21 21:32:01 -0400

    Add image to homepage rather than use a link to a website for it

--- single-use-planet-client/src/components/About.js
+++ single-use-planet-client/src/components/About.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import bins from '../images/Pawel_czerwinski_RkIsyD_AVvc_unsplash.jpg'; 
+
 
 const About = ()=> (
 			<div className="container-fluid">
 				<div className="row">
-				<div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-5 ml-3" > 
-					<h2>SINGLE-USE PLANET is a waste diversion app that helps people figure out what to do with the things that no longer serve them. We offer alternatives to sending things to landfills because let’s face it - when we throw things away there actually is no "away."</h2>
+				<div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 mt-5 ml-4 mr-4" > 
+					<h3>SINGLE-USE PLANET helps people figure out what to do with the things that no longer serve them. It offers alternatives to sending things to landfills because let’s face it - when we throw things away there actually is no "away."</h3>
 				</div>
-				<div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 mt-5 mr-3">
-					<img className="mt-3" src="https://source.unsplash.com/RkIsyD_AVvc/450x300" alt=""/>
+				<div className="col-xl-5 col-lg-5 col-md-5 col-sm-12 mt-4 mr-3">
+					<img className="homepageImage" src={bins} alt=""/>
 				</div>
 				</div>
 			</div>
 )
 
 export default About;
+
